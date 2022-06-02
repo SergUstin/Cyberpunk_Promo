9534eb0b2669cba200f04f5ce8d7d8df09f7b32d
SergUstin <s.ustin.92@mail.ru>
2022-06-02 12:18:03 +0300

    Cyperpunk_Promo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+  });
